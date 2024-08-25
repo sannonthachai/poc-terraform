8a850a7043e78badcfae15d3c99951acf30f5f8b
Sannonthachai <sannonthachai.t@globish.co.th>
2024-08-25 22:10:25 +0700

    DEV-Service-Accounts Change role for hcp-buckets-admin

--- gcp/src/dev/service_accounts/hcp_policy.tf
+++ gcp/src/dev/service_accounts/hcp_policy.tf
@@ -1,6 +1,6 @@
-resource "google_project_iam_member" "object_admin" {
+resource "google_project_iam_member" "storage_admin" {
   project = module.variables.dev.project
-  role    = "roles/storage.objectAdmin"
+  role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.hcp["hcp-buckets-admin"].email}"
 }
 
